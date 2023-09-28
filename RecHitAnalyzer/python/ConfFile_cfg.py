1c9f6346f17f7b9824bb2663f17a016343e4a7a8
Colin Crovella <cccrovella@crimson.ua.edu>
2023-09-28 15:26:17 +0200

    Also must commit ConfFile_cfg.py

--- RecHitAnalyzer/python/ConfFile_cfg.py
+++ RecHitAnalyzer/python/ConfFile_cfg.py
@@ -31,10 +31,13 @@ process.maxEvents = cms.untracked.PSet(
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-      options.inputFiles
+      #options.inputFiles
+      open('../../../list_sim_aee_hightpt.txt').readlines()
+      #'file:SIM_SinglePi0_ptgun.root'
       )
-    , skipEvents = cms.untracked.uint32(options.skipEvents)
-    )
+, skipEvents = cms.untracked.uint32(options.skipEvents)
+)
+
 print " >> Loaded",len(options.inputFiles),"input files from list."
 
 process.load("MLAnalyzer.RecHitAnalyzer.RHAnalyzer_cfi")
